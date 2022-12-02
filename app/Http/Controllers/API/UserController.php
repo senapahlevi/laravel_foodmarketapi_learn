7484ea6a25f976f0accdc8a49d978fbd2e7fcbfd
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2022-12-02 23:00:20 +0700

    register

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Actions\Fortify\PasswordValidationRules;
 
 class UserController extends Controller
 {
+    use PasswordValidationRules;
+
     //
     public function login(Request $request){
         try{
@@ -35,6 +39,43 @@ class UserController extends Controller
                 'token_type' => $Bearer,
                 'user' => $user,
             ],'Authenticated');
+        }
+        catch(Exception $error){
+            return ResponseFormatter::error([
+                'message'=>'Something went to wrong',
+                'error'=>$error,
+            ],'Authenticated failed',500);
+        }
+    }
+
+    public function register(Request $request){
+        try{
+            //validasi input login 
+            $request->validate([
+                'name' =>['required','string','max:255'],
+                'email' =>['required','string','email','max:255','unique:users'],
+                'password' =>$this->passwordRules(),
+            ]);
+         
+            User::create([
+                'name' =>$request->name,
+                'email' =>$request->email,
+                'address' =>$request->address,
+                'houseNumber' =>$request->houseNumber,
+                'phoneNumber' =>$request->phoneNumber,
+                'city' =>$request->city,
+                'password' =>Hash::make($request->password),
+            ]);
+            //setelah create simpan nih
+            $user = User::where('email',$request->email)->first();
+                    //jika berhasil maka loginkan (ambil token)
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            //kembalikan token akses,tipe dan user nya 
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => $Bearer,
+                'user' => $user,
+            ]);
         }
         catch(Exception $error){
             return ResponseFormatter::error([
